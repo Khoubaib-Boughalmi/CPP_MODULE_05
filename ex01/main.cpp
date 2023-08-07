cd8c356c2c68f90a1d42709c04fd90322d2c0034
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-08-07 15:59:26 +0100

    cleaning ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -33,28 +33,5 @@ int	main(void)
 	catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
-
-	// try {
-	// 	Bureaucrat	bureaucrat1("Bob", 7);
-	// 	Form		form1("Burn the black-hole", 0, 199);
-
-	// 	std::cout << form1 << std::endl;
-
-	// 	form1.beSigned(bureaucrat1);
-	// }
-	// catch (std::exception& e) {
-	// 	std::cout << e.what() << std::endl;
-	// }
-
-	// try {
-	// 	Bureaucrat	bureaucrat1("James", 1);
-	// 	Form		form1("End of cpp module", 2, 140);
-
-	// 	std::cout << form1 << std::endl;
-
-	// 	bureaucrat1.signForm(form1);
-	// }
-	// catch (std::exception& e) {
-	// 	std::cout << e.what() << std::endl;
-	// }
+	return (0);
 }
